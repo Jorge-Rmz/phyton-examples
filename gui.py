dcd04c7bdbd8b0787cdb40a2aa11e2bcbec91e47
Jorge-Rmz <71052252+Jorge-Rmz@users.noreply.github.com>
2024-07-03 13:44:07 -0600

    day 17---Implement The Edit button

--- gui.py
+++ gui.py
@@ -1,25 +1,42 @@
-import Funtions.functions as fun
+import Funtions.functions as func
 import PySimpleGUI as sg
 
 label = sg.Text('Type in a todo')
 
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(func.get_todos(), key="todos",
+                      enable_events=True, size=[45, 10])
+
+edit_button = sg.Button("Edit")
 
 windows = sg.Window('My to-do app',
-                    layout=[[label], [input_box, add_button]],
+                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
                     font=("Helvetica", 20))
 
 while True:
     event,values = windows.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values["todos"])
     match event:
         case "Add":
-            todos = fun.get_todos()
+            todos = func.get_todos()
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
-            fun.write_todos(todos)
+            func.write_todos(todos)
+            windows['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo= values['todo']
+            todos = func.get_todos()
+
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            func.write_todos(todos)
+            windows['todos'].update(values=todos)
+        case "todos":
+            windows['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 windows.close()
