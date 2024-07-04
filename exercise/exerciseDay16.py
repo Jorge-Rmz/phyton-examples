54b3b16567947e6ee0afda05c93d596fc06fdf15
Jorge-Rmz <71052252+Jorge-Rmz@users.noreply.github.com>
2024-07-03 23:22:40 -0600

    day 17---exercise

--- exercise/exerciseDay16.py
+++ exercise/exerciseDay16.py
@@ -1,19 +1,26 @@
 import PySimpleGUI as sg
 
+from Bonus.parsers14 import convert
 label1 = sg.Text("Enter feet: ")
-input1 = sg.Input()
+input1 = sg.Input(key="feet")
 
 
 label2 = sg.Text("Enter inches; ")
-input2 = sg.Input()
+input2 = sg.Input(key="inches")
 
 compress_button = sg.Button("Convert")
+output_label = sg.Text(key="output", text_color="white")
 
 window = sg.Window("Convertor", layout=[
                                     [label1, input1],
                                     [label2, input2],
-                                    [compress_button]],
+                                    [compress_button, output_label]],
                   )
+while True:
+    event, values = window.read()
+    print(values)
+    result = convert(feet=int(values["feet"]), inches=int(values["inches"]))
+    print(result)
+    window["output"].update(value=f"{result} m")
 
-window.read()
 window.close()
