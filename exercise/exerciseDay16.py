83f785e97ca4dc80b3a2e9b0df7009d4586f548b
Jorge-Rmz <71052252+Jorge-Rmz@users.noreply.github.com>
2024-07-04 20:19:01 -0600

    day 18----Exercise

--- exercise/exerciseDay16.py
+++ exercise/exerciseDay16.py
@@ -10,17 +10,21 @@ input2 = sg.Input(key="inches")
 
 compress_button = sg.Button("Convert")
 output_label = sg.Text(key="output", text_color="white")
+exit_button = sg.Button("Exit", key="exit")
 
 window = sg.Window("Convertor", layout=[
                                     [label1, input1],
                                     [label2, input2],
-                                    [compress_button, output_label]],
+                                    [compress_button, exit_button, output_label]],
                   )
 while True:
     event, values = window.read()
     print(values)
-    result = convert(feet=int(values["feet"]), inches=int(values["inches"]))
-    print(result)
-    window["output"].update(value=f"{result} m")
+    print(event)
+    if event == "exit" or event == sg.WIN_CLOSED:
+        break
+    if event == "Convert":
+        result = convert(feet=float(values["feet"]), inches=float(values["inches"]))
+        window["output"].update(value=f"{result} m")
 
 window.close()
